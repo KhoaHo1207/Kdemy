859629e6d355e6216820f3285364c71d27a3a6c6
Khoa <khoaho120703@gmail.com>
2025-07-06 23:01:30 +0700

    feat (educator page): add course ui

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const AddCourse = () => {
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTtitle, setCourseTitle] = useState("");
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
   return (
     <div>
       <h1>AddCourse</h1>
